8556255657c06e60d79fe1c0e3ab9693a1dc05fe
fjtoo <903621669@qq.com>
2021-12-03 19:56:38 +0800

    基本完成了http_conn类的功能实现

--- http_conn.h
+++ http_conn.h
@@ -11,18 +11,75 @@
 #include <arpa/inet.h>
 #include <sys/stat.h>
 #include <errno.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/uio.h>
 
 class http_conn
 {
 public:
     static const int READ_BUFFER_SIZE = 2048;  // 常量 读缓冲区的大小
     static const int WRITE_BUFFER_SIZE = 1024; // 常量 写缓冲区的大小
+    static const int FILENAME_LEN = 200;       // 常量 文件名的最大长度
+
+    // 以下枚举类型供HTTP请求解析的有限状态机使用，见游双书P136
+    // 分主从两个状态机，主状态机负责以行为单位解析HTTP报文，从状态机负责解析行内部的信息
+
+    // HTTP请求方法，这里只支持GET
+    enum METHOD {GET = 0, POST, HEAD, PUT, DELETE, TRACE, OPTIONS, CONNECT};
+    
+    /*
+        解析客户端请求时，主状态机的状态
+        CHECK_STATE_REQUESTLINE:当前正在分析请求行
+        CHECK_STATE_HEADER:当前正在分析头部字段
+        CHECK_STATE_CONTENT:当前正在解析请求体
+    */
+    enum CHECK_STATE { CHECK_STATE_REQUESTLINE = 0, CHECK_STATE_HEADER, CHECK_STATE_CONTENT };
+    
+    // 从状态机的三种可能状态，即行的读取状态，分别表示
+    // 1.读取到一个完整的行 2.行出错 3.行数据尚且不完整
+    enum LINE_STATUS { LINE_OK = 0, LINE_BAD, LINE_OPEN };
+
+    /*
+        服务器处理HTTP请求的可能结果，报文解析的结果
+        NO_REQUEST          :   请求不完整，需要继续读取客户数据
+        GET_REQUEST         :   表示获得了一个完成的客户请求
+        BAD_REQUEST         :   表示客户请求语法错误
+        NO_RESOURCE         :   表示服务器没有资源
+        FORBIDDEN_REQUEST   :   表示客户对资源没有足够的访问权限
+        FILE_REQUEST        :   文件请求,获取文件成功
+        INTERNAL_ERROR      :   表示服务器内部错误
+        CLOSED_CONNECTION   :   表示客户端已经关闭连接了
+    */
+    enum HTTP_CODE { NO_REQUEST, GET_REQUEST, BAD_REQUEST, NO_RESOURCE, FORBIDDEN_REQUEST, FILE_REQUEST, INTERNAL_ERROR, CLOSED_CONNECTION };
+    
+    
 
 private:
     int m_sockfd; // 该HTTP连接的套接字，如果设置为-1表示已经关闭
     sockaddr_in m_address; // 通信地址
     char m_read_buf[READ_BUFFER_SIZE];
     int m_read_index; // 表示当前读缓冲区中读入的数据最后一个字节在读缓冲区中的位置
+
+    int m_check_index;  // 当前正在解析的字符在读缓冲区位置
+    int m_start_line;   // 当前正在解析行的起始位置
+
+    CHECK_STATE m_check_state; // 主状态机的状态
+    METHOD m_method;                        // 请求方法
+
+    char m_real_file[FILENAME_LEN];         // 客户请求的目标文件的完整路径，其内容等于 doc_root + m_url, doc_root是网站根目录
+    char* m_url;                            // 客户请求的目标文件的文件名
+    char* m_version;                        // HTTP协议版本号，我们仅支持HTTP1.1
+    char* m_host;                           // 主机名
+    int m_content_length;                   // HTTP请求的消息总长度
+    bool m_linger;                          // HTTP请求是否要求保持连接
+
+    char m_write_buf[ WRITE_BUFFER_SIZE ];  // 写缓冲区
+    int m_write_idx;                        // 写缓冲区中待发送的字节数
+    char* m_file_address;                   // 客户请求的目标文件被mmap到内存中的起始位置
+    struct stat m_file_stat;                // 目标文件的状态。通过它我们可以判断文件是否存在、是否为目录、是否可读，并获取文件大小等信息
+    struct iovec m_iv[2];                   // 我们将采用writev来执行写操作，所以定义下面两个成员，其中m_iv_count表示被写内存块的数量。
+    int m_iv_count;
 
 public:
 
@@ -34,6 +91,30 @@ public:
     void close_conn(); // 关闭连接
     bool read();       // 非阻塞读
     bool write();      // 非阻塞写
+
+private:
+    void init();    // 初始化连接
+    HTTP_CODE process_read();    // 解析HTTP请求
+    bool process_write( HTTP_CODE ret );    // 填充HTTP应答
+
+    // 下面这一组函数被process_read调用以分析HTTP请求
+    HTTP_CODE parse_request_line( char* text );
+    HTTP_CODE parse_headers( char* text );
+    HTTP_CODE parse_content( char* text );
+    HTTP_CODE do_request();
+    char* get_line() { return m_read_buf + m_start_line; }
+    LINE_STATUS parse_line();
+
+    // 这一组函数被process_write调用以填充HTTP应答。
+    void unmap();
+    bool add_response( const char* format, ... );
+    bool add_content( const char* content );
+    bool add_content_type();
+    bool add_status_line( int status, const char* title );
+    bool add_headers( int content_length );
+    bool add_content_length( int content_length );
+    bool add_linger();
+    bool add_blank_line();
 };
 
 
